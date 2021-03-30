67fdeda32e12d62f4bb6e5950726213d205094ee
Corona Laufer <laufer.corona@hotmail.com>
2021-03-30 16:02:04 +0200

    refactor: dashboard/proposal component

--- src/components/dashbord/Dashboard.tsx
+++ src/components/dashbord/Dashboard.tsx
@@ -2,14 +2,15 @@ import React from 'react';
 import { makeStyles, useTheme, Theme, createStyles } from '@material-ui/core/styles';
 import './Dashboard.css'
 import DashButton from './dashcontent/DashButtons';
+import ProposalCard from './Proposals/proposals';
 
 
 
 
 export default function Dashboard() {
   return (
-    <div className='dashContainer'>
-      <DashButton/>
+    <div className='dash-container'>
+      <ProposalCard />
     </div>
   );
 }
--- src/components/dashbord/dashcontent/DashButtons.tsx
+++ src/components/dashbord/dashcontent/DashButtons.tsx
@@ -9,16 +9,16 @@ import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import './DashButton.css'
 import ReportCard from './reportsButton';
-import Progressbar from './ProgressBar'
+
 import { Link } from 'react-router-dom';
 const useStyles = makeStyles({
   root: {
     maxWidth: 345,
-    
+
   },
   img1: {
     height: 200,
-    width:200
+    width: 200
   }
 });
 
@@ -28,84 +28,84 @@ export default function DashButton() {
   return (
     <div className='buttonContainer'>
 
-    <div>
-    <Card className={classes.root}>
-      <CardActionArea >
-        <CardContent>
-            <Typography gutterBottom variant="h5" component="h2">
-             Proposals   
+      <div>
+        <Card className={classes.root}>
+          <CardActionArea >
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                Proposals
             </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            this area is for the lastest updates of something
+              <Typography variant="body2" color="textSecondary" component="p">
+                this area is for the lastest updates of something
           </Typography>
-        </CardContent>
-      </CardActionArea>
-        <CardActions>
-        <Link to='/LogIn'>   
-          <Button variant="contained" color="primary">
-            Newest Proposal
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <Link to='/LogIn'>
+              <Button variant="contained" color="primary">
+                Newest Proposal
           </Button>
-        </Link>
-      </CardActions>
-      </Card>
-        </div>
-        <div>
-      <Card className={classes.root}>
-        <CardActionArea>
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="h2">
-            Traffic Reports
-            </Typography>
-            <Typography variant="body2" color="textSecondary" component="p">
-            Review the lastest traffic and send out an alert!
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-        <CardActions>
-        <Button variant="contained" color="primary">
-        Send Alert
-        </Button>
-      </CardActions>
-      </Card>
+            </Link>
+          </CardActions>
+        </Card>
       </div>
-      <div> 
-      <Card className={classes.root}>
-      <CardActionArea>
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-           Lastest Weather reports 
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-           see the weather
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button variant="contained" color="primary">
-         Send an Alert
+      <div>
+        <Card className={classes.root}>
+          <CardActionArea>
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                Traffic Reports
+            </Typography>
+              <Typography variant="body2" color="textSecondary" component="p">
+                Review the lastest traffic and send out an alert!
+            </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <Button variant="contained" color="primary">
+              Send Alert
         </Button>
-      </CardActions>
-      </Card>
-        </div>
-      <ReportCard/>
+          </CardActions>
+        </Card>
+      </div>
+      <div>
+        <Card className={classes.root}>
+          <CardActionArea>
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                Lastest Weather reports
+          </Typography>
+              <Typography variant="body2" color="textSecondary" component="p">
+                see the weather
+          </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <Button variant="contained" color="primary">
+              Send an Alert
+        </Button>
+          </CardActions>
+        </Card>
+      </div>
+      <ReportCard />
       <div style={{ width: 290, height: 350 }}>
-        <Progressbar />
-      </div>
-        <div >
-     <Card className={classes.root}>
-      <CardActionArea>
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-           Polls
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-          Quick view to all active polls           
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      </Card>
 
       </div>
+      <div >
+        <Card className={classes.root}>
+          <CardActionArea>
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                Polls
+          </Typography>
+              <Typography variant="body2" color="textSecondary" component="p">
+                Quick view to all active polls
+          </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+
       </div>
+    </div>
   );
 }
